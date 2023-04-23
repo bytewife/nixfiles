75095bf31297c086af1d76d8584629b5b7c87939
Ivy Raine <ivy.ember.raine@gmail.com>
2023-04-23 05:23:13 +0000

    Update SSH

--- configurations/rpi.nix
+++ configurations/rpi.nix
@@ -5,4 +5,10 @@ imports =
       "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
       ./configuration.nix
     ];
+    networking.interfaces.wlan0.ipv4.addresses = [{
+	# Note: This address (of course) must be a valid address w.r.t. subnet mask.
+	# Can be found with `ifconfig <interface>`.wlan0 is one such interface.
+	address = "10.0.0.168";
+	prefixLength = 24;
+    }];
 }
