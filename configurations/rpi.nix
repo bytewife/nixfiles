7478ba6bc2675e730fb3d9d8629a5f945c019236
Ivy Raine <ivyemberraine@gmail.com>
2023-08-02 22:20:49 -0700

    Organize

--- configurations/rpi.nix
+++ configurations/rpi.nix
@@ -1,14 +1,37 @@
 { modulesPath, ... }:
 {
 imports =
-    [
-      "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
-      ./configuration.nix
-    ];
-    networking.interfaces.wlan0.ipv4.addresses = [{
-	# Note: This address (of course) must be a valid address w.r.t. subnet mask.
-	# Can be found with `ifconfig <interface>`.wlan0 is one such interface.
-	address = "10.0.0.168";
-	prefixLength = 24;
-    }];
+  [
+    "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
+    ./configuration.nix
+  ];
+  networking.interfaces.wlan0.ipv4.addresses = [{
+    # Note: This address (of course) must be a valid address w.r.t. subnet mask.
+    # Can be found with `ifconfig <interface>`.wlan0 is one such interface.
+    address = "10.0.0.168";
+    prefixLength = 24;
+  }];
+  services = {
+    syncthing = {
+      configDir = "/home/ivy/.config/syncthing";
+      dataDir = "/home/ivy/";
+      enable = true;
+      user = "ivy";
+      group = "users";
+      openDefaultPorts = true;
+      devices = {
+        "ivy777" = { id = "L6F6DXN-3I6SUMU-QE27QUE-GI67GOI-DNWOSWF-JLDAKMK-PMQ57VG-2SQ4HAM"; };
+      };
+      folders = {
+        # This is the name of the folder in Syncthing, as well as the folder ID.
+        "Sync" = {
+          # This is the folder to Sync from this device.
+          path = "/home/ivy/Sync";
+          devices = [ "ivy777" ];
+          versioning.type = "simple";
+          versioning.params.keep = "10";
+        };
+      };
+    };
+  };
 }
